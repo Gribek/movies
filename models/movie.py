f0a881a2a6b0e766bbe8f7abf12e0a137ad89301
Piotr Maslewski <piotrekmaslewski@gmail.com>
2019-10-30 17:49:07 +0100

    Added method that allows the database entries to be updated.

--- models/movie.py
+++ models/movie.py
@@ -20,3 +20,14 @@ class Movie:
     @property
     def id(self):
         return self.__id
+
+    def update(self, cursor):
+        sql = """UPDATE MOVIES SET title=?, year=?, runtime=?, genre=?,
+        director=?, cast=?, writer=?, language=?, country=?, awards=?,
+        imdb_rating=?, imdb_votes=?, box_office=? WHERE id=?"""
+        values = (self.title, self.year, self.runtime, self.genre,
+                  self.director, self.cast, self.writer, self.language,
+                  self.country, self.awards, self.imdb_rating,
+                  self.imdb_votes, self.box_office, self.id)
+        cursor.execute(sql, values)
+        return True
