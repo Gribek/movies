561b7f909ffe39cca516f99748a745922f94fe46
Piotr Maslewski <piotrekmaslewski@gmail.com>
2019-11-01 16:54:20 +0100

    Created movies.py. Added cli commands usign argparse.

---
+++ movies.py
@@ -0,0 +1,46 @@
+import argparse
+
+from movie_functions import sort_movies
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+# sort command
+parser_sort = subparsers.add_parser('sort')
+parser_sort.add_argument('column', nargs='+', choices=[
+    'year', 'runtime', 'genre', 'director', 'cast', 'writer', 'language',
+    'country', 'awards', 'imdb_rating', 'imdb_votes', 'box_office', 'title'])
+parser_sort.set_defaults(function=sort_movies)
+
+# filter_by command
+parser_filter_by = subparsers.add_parser('filter_by')
+subparsers_filter_by = parser_filter_by.add_subparsers()
+
+parser_filter_by_parameter = subparsers_filter_by.add_parser('parameter')
+parser_filter_by_parameter.add_argument('parameter', choices=[
+    'actor', 'director', 'language'])
+parser_filter_by_parameter.add_argument('value')
+parser_filter_by_parameter.set_defaults()
+
+parser_filter_by_movie_info = subparsers_filter_by.add_parser('movie')
+parser_filter_by_movie_info.add_argument('movie_info', choices=[
+    'oscar_nominated_no_win', 'high_awards_win_rate', 'top_box_office'])
+parser_filter_by_movie_info.set_defaults()
+
+# compare command
+parser_compare = subparsers.add_parser('compare')
+parser_compare.add_argument('column', choices=['imdb_rating', 'box_office', 'awards_won', 'runtime'])
+parser_compare.add_argument('movie', nargs=2)
+parser_compare.set_defaults()
+
+# add command
+parser_add = subparsers.add_parser('add')
+parser_add.add_argument('movie_title')
+parser_add.set_defaults()
+
+# highscores command
+parser_highscores = subparsers.add_parser('highscores')
+parser_highscores.set_defaults()
+
+args = parser.parse_args()
+args.function(args)
