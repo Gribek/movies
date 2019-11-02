b9f1df69e4ece61debbd6a7068f8716b7e9ff61a
Piotr Maslewski <piotrekmaslewski@gmail.com>
2019-11-02 11:38:32 +0100

    Moved functions from fill database script to movie_functions.py.

--- movie_functions.py
+++ movie_functions.py
@@ -1,3 +1,7 @@
+from urllib import request, parse
+import json
+import re
+
 from database.database_connection import connection
 from models.movie import Movie
 from operator import attrgetter
@@ -79,3 +83,57 @@ def runtime_convert_to_integer(iterable):
     for movie in iterable:
         runtime = getattr(movie, 'runtime')
         setattr(movie, 'runtime', int(runtime.split(' ')[0]))
+
+
+def get_data_from_api(movie_obj, api_url, api_key):
+    """Get data from api and save them in the movie object."""
+    url = api_url + parse.urlencode({'apikey': api_key, 't': movie_obj.title})
+    api_data = request.urlopen(url).read()
+    json_data = json.loads(api_data)
+    data_to_collect = {
+        'Title': None, 'Year': None, 'Runtime': None, 'Genre': None,
+        'Director': None, 'Actors': None, 'Writer': None, 'Language': None,
+        'Country': None, 'Awards': None, 'imdbRating': None,
+        'imdbVotes': None, 'BoxOffice': None
+    }
+    # getting information about movie from json
+    if json_data['Response'] == 'True':
+        for key in data_to_collect.keys():
+            try:
+                if json_data[key] != "N/A":
+                    data_to_collect[key] = json_data[key]
+            except KeyError:
+                pass
+        # editing movie object with collected data
+        movie_obj.title = data_to_collect['Title']
+        movie_obj.runtime = data_to_collect['Runtime']
+        movie_obj.genre = data_to_collect['Genre']
+        movie_obj.director = data_to_collect['Director']
+        movie_obj.cast = data_to_collect['Actors']
+        movie_obj.writer = data_to_collect['Writer']
+        movie_obj.language = data_to_collect['Language']
+        movie_obj.country = data_to_collect['Country']
+        movie_obj.awards = data_to_collect['Awards']
+        try:
+            movie_obj.year = convert_to_int(data_to_collect['Year'])
+        except TypeError:
+            pass
+        try:
+            movie_obj.imdb_rating = float(data_to_collect['imdbRating'])
+        except TypeError:
+            pass
+        try:
+            movie_obj.imdb_votes = convert_to_int(data_to_collect['imdbVotes'])
+        except TypeError:
+            pass
+        try:
+            movie_obj.box_office = convert_to_int(data_to_collect['BoxOffice'])
+        except TypeError:
+            pass
+    else:
+        print(f'Movie: {movie_obj.title} not found.')
+
+
+def convert_to_int(value):
+    """Remove non digit characters and convert to integer."""
+    return int(re.sub(r'\D', '', value))
