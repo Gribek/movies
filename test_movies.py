77595587956ae704a5fcb4ca2cae04598afc47dc
Piotr Maslewski <piotrekmaslewski@gmail.com>
2019-11-03 22:38:04 +0100

    Added tests that tests print_result function.

--- test_movies.py
+++ test_movies.py
@@ -3,7 +3,7 @@ import pytest
 from movie_functions import replace_underscores, create_awards_dict, \
     key_with_max_value, runtime_convert_to_integer, none_to_zero, \
     none_to_empty_string, box_office_none_to_zero, prepare_result, \
-    convert_to_int
+    convert_to_int, print_results
 from models.movie import Movie
 
 
@@ -18,19 +18,21 @@ def movie_list():
     movie_list[1].awards = 'Nominated for 2 Oscars. Another 11 wins & 16 nominations.'
     movie_list[2].awards = '7 wins & 27 nominations.'
     movie_list[0].runtime = '123 min'
-    attr_none = ['genre', 'director', 'cast', 'writer', 'language', 'country',
-                 'awards']
-    attr_zero = ['year', 'imdb_rating', 'imdb_votes', 'box_office']
-    for i in attr_none:
-        setattr(movie_list[3], i, None)
-    for i in attr_zero:
+    attr = ['genre', 'director', 'cast', 'writer', 'language', 'country',
+            'awards', 'year', 'imdb_rating', 'imdb_votes', 'box_office']
+    for i in attr:
         setattr(movie_list[3], i, None)
     return movie_list
 
 
 @pytest.fixture
 def columns():
-    return ['runtime', 'year']
+    return ['runtime', 'imdb_rating']
+
+
+@pytest.fixture
+def result():
+    return [['Inception', 111, 8.8], ['Die Hard', 144, 7.6]]
 
 
 def test_replace_underscores():
@@ -127,3 +129,22 @@ def test_convert_to_int():
     assert type(convert_to_int('dsa23e$')) is int
     assert convert_to_int('fds1@2!3$d4sdf-  re5') == 12345
     assert convert_to_int('123') == 123
+
+
+def test_if_print_results_prints_all_data(capfd, columns, result):
+    print_results(columns, result)
+    out, err = capfd.readouterr()
+    assert 'Inception' in out
+    assert 'Die Hard' in out
+    assert '111' in out
+    assert '144' in out
+    assert '8.8' in out
+    assert '7.6' in out
+
+
+def test_if_print_results_prints_headers_correctly(capfd, columns, result):
+    print_results(columns, result)
+    out, err = capfd.readouterr()
+    assert 'TITLE' in out
+    assert 'IMDB RATING' in out
+    assert 'RUNTIME' in out
