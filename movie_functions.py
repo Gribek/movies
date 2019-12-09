c71ce39b14eaeef23fc714c1bd0f042c0b78de66
Piotr Maslewski <piotrekmaslewski@gmail.com>
2019-12-09 19:20:39 +0100

    Removed redundant function.

--- movie_functions.py
+++ movie_functions.py
@@ -249,60 +249,6 @@ def print_results(columns, result, first_col='TITLE', column_wide=10):
         print(template.format(*raw))
 
 
-def get_data_from_api(movie_obj, api_url, api_key):
-    """Get data from api and save them in the movie object."""
-    url = api_url + parse.urlencode({'apikey': api_key, 't': movie_obj.title})
-    api_data = request.urlopen(url).read()
-    json_data = json.loads(api_data)
-    data_to_collect = {
-        'Title': None, 'Year': None, 'Runtime': None, 'Genre': None,
-        'Director': None, 'Actors': None, 'Writer': None, 'Language': None,
-        'Country': None, 'Awards': None, 'imdbRating': None,
-        'imdbVotes': None, 'BoxOffice': None, 'imdbID': None
-    }
-    # getting information about movie from json
-    if json_data['Response'] == 'True':
-        for key in data_to_collect.keys():
-            try:
-                if json_data[key] != "N/A":
-                    data_to_collect[key] = json_data[key]
-            except KeyError:
-                pass
-        # editing movie object with collected data
-        movie_obj.title = data_to_collect['Title']
-        movie_obj.runtime = convert_to_int(data_to_collect['Runtime'])
-        movie_obj.genre = data_to_collect['Genre']
-        movie_obj.director = data_to_collect['Director']
-        movie_obj.actors = data_to_collect['Actors']
-        movie_obj.writer = data_to_collect['Writer']
-        movie_obj.language = data_to_collect['Language']
-        movie_obj.country = data_to_collect['Country']
-        movie_obj.awards = data_to_collect['Awards']
-        movie_obj.imdb_id = data_to_collect['imdbID']
-
-        awards = data_to_collect['Awards']
-        regex = {'oscars_won': r'won (\d+) oscar', 'awards_won': r'(\d+) wins',
-                 'oscar_nominations': r'nominated for (\d+) oscar',
-                 'award_nominations': r'(\d+) nomination'}
-        for key in regex:
-            awards_number = re.search(regex[key], awards, re.I)
-            if awards_number is not None:
-                setattr(movie_obj, key, int(awards_number.group(1)))
-
-        movie_obj.year = convert_to_int(data_to_collect['Year'])
-        movie_obj.imdb_votes = convert_to_int(data_to_collect['imdbVotes'])
-        movie_obj.box_office = convert_to_int(data_to_collect['BoxOffice'])
-        try:
-            movie_obj.imdb_rating = float(data_to_collect['imdbRating'])
-        except TypeError:
-            pass
-        except ValueError:
-            pass
-        return True
-    else:
-        return False
-
-
 class OmdbApiResponse:
     """The class that represents the response from OMDb API.
 
