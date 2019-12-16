c94b261fcb83ab212cbbf918d53db3e54401bf66
Piotr Maslewski <piotrekmaslewski@gmail.com>
2019-12-16 16:09:55 +0100

    Added new sort options accordingly to new Movie model.

--- movies.py
+++ movies.py
@@ -1,6 +1,6 @@
 import argparse
 
-from movie_functions import sort_movies, compare_movies, filter_by_parameter,\
+from movie_functions import sort_movies, compare_movies, filter_by_parameter, \
     filter_by_movie_info, add_new_movie, high_scores
 
 parser = argparse.ArgumentParser()
@@ -9,8 +9,9 @@ subparsers = parser.add_subparsers()
 # sort command
 parser_sort = subparsers.add_parser('sort')
 parser_sort.add_argument('column', nargs='+', choices=[
-    'year', 'runtime', 'genre', 'director', 'cast', 'writer', 'language',
-    'country', 'awards_won', 'imdb_rating', 'imdb_votes', 'box_office'])
+    'year', 'runtime', 'genre', 'director', 'actors', 'writer', 'language',
+    'country', 'awards_won', 'award_nominations', 'oscar_nominations',
+    'oscars_won', 'imdb_rating', 'imdb_votes', 'box_office'])
 parser_sort.add_argument('-d', '--order', action='store_true')
 parser_sort.set_defaults(function=sort_movies)
 
@@ -31,7 +32,8 @@ parser_filter_by_movie_info.set_defaults(function=filter_by_movie_info)
 
 # compare command
 parser_compare = subparsers.add_parser('compare')
-parser_compare.add_argument('column', choices=['imdb_rating', 'box_office', 'awards_won', 'runtime'])
+parser_compare.add_argument(
+    'column', choices=['imdb_rating', 'box_office', 'awards_won', 'runtime'])
 parser_compare.add_argument('movie_title', nargs=2)
 parser_compare.set_defaults(function=compare_movies)
 
